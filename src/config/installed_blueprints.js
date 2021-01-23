39d61b6144b435cd0ab39a5e3655b640f1467511
evan-carlsen <60202545+evan-carlsen@users.noreply.github.com>
2021-01-22 19:23:48 -0800

    Update installed_blueprints.js

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -2,7 +2,8 @@ export const installed_blueprints = [
   //@BlueprintInsertion
 { name: 'UserProfile4195462', human_name: 'User Profile 4', access_route: 'UserProfile4195462'},
 { name: 'Maps7195459', human_name: 'Maps 7', access_route: 'Maps7195459', icon: 'map'},
-
+{ name: 'TermsAndConditions8195458', human_name: 'Terms & Conditions', access_route: 'TermsAndConditions8195458'},
+  
   // you can add more installed blueprints here
   // access route is the route nate given to navigator
 ];
