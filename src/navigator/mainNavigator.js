916f77c42a971b0cea96144647c3f7880bc3034c
evan-carlsen <60202545+evan-carlsen@users.noreply.github.com>
2021-01-22 18:39:04 -0800

    Update mainNavigator.js

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -7,6 +7,8 @@ import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import UserProfile4195462Navigator from '../features/UserProfile4195462/navigator';
 import Maps7195459Navigator from '../features/Maps7195459/navigator';
+import SplashScreen1195465Navigator from '../features/SplashScreen1195465navigator';
+//onboarding, login, terms, privacy, articles
 
 /**
  * new navigators can be imported here
@@ -17,7 +19,10 @@ const AppNavigator = {
     //@BlueprintNavigationInsertion
 UserProfile4195462: { screen: UserProfile4195462Navigator },
 Maps7195459: { screen: Maps7195459Navigator },
-
+    
+//onboarding, login, terms, privacy, articles
+SplashScreen1195465: { screen: SplashScreen1195465Navigator },
+    
     /** new navigators can be added here */
     SplashScreen: {
       screen: SplashScreen
@@ -29,7 +34,8 @@ const DrawerAppNavigator = createDrawerNavigator(
     ...AppNavigator,
   },
   {
-    contentComponent: SideMenu
+    contentComponent: SideMenu,
+    initialRouteName: "SplashScreen1195465", // Splash Screen
   },
 );
 
